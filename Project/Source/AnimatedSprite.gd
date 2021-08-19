595d045c8759c9e9716f2220f16b8f8f2f79aa2a
Zezeku <48072285+Zezeku@users.noreply.github.com>
2021-08-19 11:24:44 -0400

    TallGrass
    MAde a tall grass object, that animates when player runs into it. Each tall grass uses area 2d and signals. Added 50 objects, runs smoothly as of now

--- Project/Source/AnimatedSprite.gd
+++ Project/Source/AnimatedSprite.gd
@@ -1,20 +1,20 @@
-extends AnimatedSprite
+extends KinematicBody2D
 
 var speed = 16 ;
 var move_dir = Vector2();
 
 func get_input():
-	
+	print(position)
 	move_dir = Vector2();
 	
 	if Input.is_action_just_pressed("ui_right"):
-		move_dir.x += 1 ;
+		move_dir.x += 0.25 ;
 	if Input.is_action_just_pressed("ui_left"):
-		move_dir.x -= 1 ;
+		move_dir.x -= 0.25 ;
 	if Input.is_action_just_pressed("ui_up"):
-		move_dir.y -= 1 ;
+		move_dir.y -= 0.25 ;
 	if Input.is_action_just_pressed("ui_down"):
-		move_dir.y += 1 ;
+		move_dir.y += 0.25 ;
 		
 	
 	
@@ -22,3 +22,4 @@ func get_input():
 func _physics_process(delta):
 	get_input()
 	translate(move_dir*speed) ;
+	
---
+++ Project/Source/TallGrass.gd
@@ -0,0 +1,29 @@
+extends Area2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Node2D_body_entered(body):
+	$Sprite.visible = false;
+	$AnimatedSprite.frame = 0;
+	$AnimatedSprite.play("default");
+	$AnimatedSprite.visible = true ;
+	
+
+
+func _on_AnimatedSprite_animation_finished():
+	$Sprite.visible = true ;
+	$AnimatedSprite.visible = false ;
