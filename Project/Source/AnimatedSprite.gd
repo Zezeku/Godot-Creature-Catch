5457c529304ab80273a36b2c673bb379a89df317
Zezeku <48072285+Zezeku@users.noreply.github.com>
2021-08-27 10:12:28 -0400

    updated running*
    cleaned up code with smarter functions

--- Project/Source/AnimatedSprite.gd
+++ Project/Source/AnimatedSprite.gd
@@ -1,21 +1,21 @@
 extends KinematicBody2D
 
-var friction = 400 ;
-var acceleration = 150 ;
-var max_speed = 50 ;
+var friction = 500 ;
+var acceleration = 500 ;
+var max_speed = 60 ;
 var velocity = Vector2.ZERO;
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO ;
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left") ;
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up") ;
+	input_vector = input_vector.normalized();
 	
 	if input_vector != Vector2.ZERO :
-		velocity += input_vector.normalized() * (acceleration * delta) ;
-		velocity = velocity.clamped(max_speed * delta)
+		velocity = velocity.move_toward(input_vector * max_speed, acceleration * delta) ;
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, friction * delta) ;
 	
-	move_and_collide(velocity) ;
+	move_and_collide(velocity * delta) ;
 	
 	
